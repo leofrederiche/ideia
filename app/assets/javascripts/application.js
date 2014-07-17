15a6e1bbec19b99737333ae08dab58a03ee583c8
Designa <designa@designa-Vostro-1400.(none)>
2014-07-17 10:22:26 -0300

    Status collaborate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,10 +23,14 @@ function openMenu(){
 	{
 		document.getElementById("user_menu").style.display = "block";
 		document.getElementById("arrow_down_menu").style.webkitTransform = 'rotate('+180+'deg)'; 
+		document.getElementById("arrow_down_menu").style.mozTransform = 'rotate('+180+'deg)'; 
+		document.getElementById("arrow_down_menu").style.msTransform = 'rotate('+180+'deg)'; 
 	}
 	else{
 		document.getElementById("user_menu").style.display = "none";	
 		document.getElementById("arrow_down_menu").style.webkitTransform = 'rotate('+0+'deg)'; 
+		document.getElementById("arrow_down_menu").style.mozTransform = 'rotate('+0+'deg)'; 
+		document.getElementById("arrow_down_menu").style.msTransform = 'rotate('+0+'deg)'; 
 	}
 }
 
